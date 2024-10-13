ca61099c69d316bf77384df132e508185218205e
Ted Schundler <tschundler@gmail.com>
2024-10-13 00:17:41 -0700

    Updated timings for ws28xx/sk68xx & clones.
    The existing timings don't work reliably with all my various ws28xx clones. This set of timings is more reliable.
    
    I added some notes about constraints I discovered. It seems WLED uses 400ns/800ns, which is below datasheet values, but within those discovered constraints. FastLED uses similar to the previous values here (300/640?) and is also glitchy on some of my LEDs than need >= 350ns T0H to work reliably.

--- esp-hal-smartled/src/lib.rs
+++ esp-hal-smartled/src/lib.rs
@@ -34,10 +34,10 @@ use esp_hal::{
 };
 use smart_leds_trait::{SmartLedsWrite, RGB8};
 
-const SK68XX_CODE_PERIOD: u32 = 1200;
-const SK68XX_T0H_NS: u32 = 320;
+const SK68XX_CODE_PERIOD: u32 = 1250; // 800kHz
+const SK68XX_T0H_NS: u32 = 400; // 300ns per SK6812 datasheet, 400 per WS2812. Some require >350ns for T0H. Others <500ns for T0H.
 const SK68XX_T0L_NS: u32 = SK68XX_CODE_PERIOD - SK68XX_T0H_NS;
-const SK68XX_T1H_NS: u32 = 640;
+const SK68XX_T1H_NS: u32 = 850; // 900ns per SK6812 datasheet, 850 per WS2812. > 550ns is sometimes enough. Some require T1H >= 2 * T0H. Some require > 300ns T1L.
 const SK68XX_T1L_NS: u32 = SK68XX_CODE_PERIOD - SK68XX_T1H_NS;
 
 /// All types of errors that can happen during the conversion and transmission
