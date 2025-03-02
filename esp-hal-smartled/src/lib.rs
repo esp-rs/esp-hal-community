1f8ab9af6e6b8773cdf8015334c468ab7a61c428
taorye <taorye@outlook.com>
2025-03-02 23:52:49 +0800

    fix(smartled): Update to esp-hal 1.0.0-beta.0

--- esp-hal-smartled/src/lib.rs
+++ esp-hal-smartled/src/lib.rs
@@ -27,7 +27,7 @@ use core::{fmt::Debug, slice::IterMut};
 
 use esp_hal::{
     clock::Clocks,
-    gpio::OutputPin,
+    gpio::{OutputPin, Level},
     peripheral::Peripheral,
     rmt::{Error as RmtError, PulseCode, TxChannel, TxChannelConfig, TxChannelCreator},
 };
@@ -99,35 +99,32 @@ where
         O: OutputPin + 'd,
         C: TxChannelCreator<'d, TX, O>,
     {
-        let config = TxChannelConfig {
-            clk_divider: 1,
-            idle_output_level: false,
-            carrier_modulation: false,
-            idle_output: true,
-
-            ..TxChannelConfig::default()
-        };
+        let config = TxChannelConfig::default()
+            .with_clk_divider(1)
+            .with_idle_output_level(Level::Low)
+            .with_carrier_modulation(false)
+            .with_idle_output(true);
 
         let channel = channel.configure(pin, config).unwrap();
 
         // Assume the RMT peripheral is set up to use the APB clock
         let clocks = Clocks::get();
-        let src_clock = clocks.apb_clock.to_MHz();
+        let src_clock = clocks.apb_clock.as_mhz();
 
         Self {
             channel: Some(channel),
             rmt_buffer,
             pulses: (
                 PulseCode::new (
-                    true,
+                    Level::High,
                     ((SK68XX_T0H_NS * src_clock) / 1000) as u16,
-                    false,
+                    Level::Low,
                     ((SK68XX_T0L_NS * src_clock) / 1000) as u16,
                 ),
                 PulseCode::new (
-                    true,
+                    Level::High,
                     ((SK68XX_T1H_NS * src_clock) / 1000) as u16,
-                    false,
+                    Level::Low,
                     ((SK68XX_T1L_NS * src_clock) / 1000) as u16,
                 ),
             ),
