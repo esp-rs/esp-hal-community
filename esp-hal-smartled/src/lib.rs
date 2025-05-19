23c90b5110c209327c6f6a89d4b5a30307be7d49
James Sizeland <jamessizeland@gmail.com>
2025-05-19 23:00:17 +0100

    complete docs example

--- esp-hal-smartled/src/lib.rs
+++ esp-hal-smartled/src/lib.rs
@@ -10,11 +10,27 @@
 //!
 //! ## Example
 //!
-//! ```rust,ignore
-//! let rmt = Rmt::new(peripherals.RMT, Rate::from_mhz(80)).unwrap();
+//! ```rust
+//! #![no_std]
+//! #![no_main]
 //!
-//! let rmt_buffer = smartLedBuffer!(1);
-//! let mut led = SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO8, rmt_buffer);
+//! use esp_backtrace as _;
+//! use esp_hal::{rmt::Rmt, time::Rate, Config};
+//! use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
+//! use smart_leds::{brightness, colors::RED, SmartLedsWrite as _};
+//!
+//! #[esp_hal::main]
+//! fn main() -> ! {
+//!     let p = esp_hal::init(Config::default());
+//!     let mut led = {
+//!         let frequency = Rate::from_mhz(80);
+//!         let rmt = Rmt::new(p.RMT, frequency).expect("Failed to initialize RMT0");
+//!         SmartLedsAdapter::new(rmt.channel0, p.GPIO2, smartLedBuffer!(1))
+//!     };
+//!     let level = 10;
+//!     led.write(brightness([RED].into_iter(), level)).unwrap();
+//!     loop {} // loop forever
+//! }
 //! ```
 //!
 //! ## Feature Flags
