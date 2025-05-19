471e63950e77d6cabda9bc6b124ac19907fbc41c
James Sizeland <jamessizeland@gmail.com>
2025-05-19 23:07:17 +0100

    tidy up hello_rgb example

--- esp-hal-smartled/examples/hello_rgb.rs
+++ esp-hal-smartled/examples/hello_rgb.rs
@@ -11,12 +11,17 @@
 //! The following wiring is assumed for ESP32:
 //! - LED => GPIO33
 //! The following wiring is assumed for ESP32C3:
-//! - LED => GPIO8
+//! <https://github.com/esp-rs/esp-rust-board/tree/master>
+//! - LED => GPIO2
 //! The following wiring is assumed for ESP32C6, ESP32H2:
+//! <https://docs.espressif.com/projects/esp-dev-kits/en/latest/esp32c6/esp32-c6-devkitm-1/user_guide.html>
+//! <https://docs.espressif.com/projects/esp-dev-kits/en/latest/esp32h2/esp32-h2-devkitm-1/user_guide.html>
 //! - LED => GPIO8
 //! The following wiring is assumed for ESP32S2:
+//! <https://docs.espressif.com/projects/esp-dev-kits/en/latest/esp32s2/esp32-s2-devkitm-1/user_guide.html>
 //! - LED => GPIO18
 //! The following wiring is assumed for ESP32S3:
+//! <https://docs.espressif.com/projects/esp-dev-kits/en/latest/esp32s3/esp32-s3-devkitm-1/user_guide.html>
 //! - LED => GPIO48
 
 #![no_std]
@@ -28,44 +33,47 @@ use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
 use smart_leds::{
     brightness, gamma,
     hsv::{hsv2rgb, Hsv},
-    SmartLedsWrite,
+    SmartLedsWrite, RGB8,
 };
 
 #[main]
 fn main() -> ! {
-    let peripherals = esp_hal::init(esp_hal::Config::default());
+    // Initialize the HAL Peripherals
+    let p = esp_hal::init(esp_hal::Config::default());
 
     // Each devkit uses a unique GPIO for the RGB LED, so in order to support
     // all chips we must unfortunately use `#[cfg]`s:
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "esp32")] {
-            let led_pin = peripherals.GPIO33;
-        } else if #[cfg(feature = "esp32c3")] {
-            let led_pin = peripherals.GPIO8;
-        } else if #[cfg(any(feature = "esp32c6", feature = "esp32h2"))] {
-            let led_pin = peripherals.GPIO8;
-        } else if #[cfg(feature = "esp32s2")] {
-            let led_pin = peripherals.GPIO18;
-        } else if #[cfg(feature = "esp32s3")] {
-            let led_pin = peripherals.GPIO48;
+    let mut led: SmartLedsAdapter<_, 25> = {
+        // Configure RMT peripheral globally
+        let frequency: Rate = {
+            cfg_if::cfg_if! {
+                if #[cfg(feature = "esp32h2")] {
+                    Rate::from_mhz(32)
+                } else {
+                    Rate::from_mhz(80)
+                }
+            }
+        };
+        // We use one of the RMT channels to instantiate a `SmartLedsAdapter` which can
+        // be used directly with all `smart_led` implementations
+        let rmt_channel = Rmt::new(p.RMT, frequency)
+            .expect("Failed to initialize RMT0")
+            .channel0;
+        let rmt_buffer = smartLedBuffer!(1);
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "esp32")] {
+                SmartLedsAdapter::new(rmt_channel, p.GPIO33, rmt_buffer)
+            } else if #[cfg(feature = "esp32c3")] {
+                SmartLedsAdapter::new(rmt_channel, p.GPIO2, rmt_buffer)
+            } else if #[cfg(any(feature = "esp32c6", feature = "esp32h2"))] {
+                SmartLedsAdapter::new(rmt_channel, p.GPIO8, rmt_buffer)
+            } else if #[cfg(feature = "esp32s2")] {
+                SmartLedsAdapter::new(rmt_channel, p.GPIO18, rmt_buffer)
+            } else if #[cfg(feature = "esp32s3")] {
+                SmartLedsAdapter::new(rmt_channel, p.GPIO48, rmt_buffer)
+            }
         }
-    }
-
-    // Configure RMT peripheral globally
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "esp32h2")] {
-            let freq = Rate::from_mhz(32);
-        } else {
-            let freq = Rate::from_mhz(80);
-        }
-    }
-
-    let rmt = Rmt::new(peripherals.RMT, freq).unwrap();
-
-    // We use one of the RMT channels to instantiate a `SmartLedsAdapter` which can
-    // be used directly with all `smart_led` implementations
-    let rmt_buffer = smartLedBuffer!(1);
-    let mut led = SmartLedsAdapter::new(rmt.channel0, led_pin, rmt_buffer);
+    };
 
     let delay = Delay::new();
 
@@ -74,7 +82,7 @@ fn main() -> ! {
         sat: 255,
         val: 255,
     };
-    let mut data;
+    let mut data: RGB8;
 
     loop {
         // Iterate over the rainbow!
@@ -82,11 +90,11 @@ fn main() -> ! {
             color.hue = hue;
             // Convert from the HSV color space (where we can easily transition from one
             // color to the other) to the RGB color space that we can then send to the LED
-            data = [hsv2rgb(color)];
+            data = hsv2rgb(color);
             // When sending to the LED, we do a gamma correction first (see smart_leds
             // documentation for details) and then limit the brightness to 10 out of 255 so
             // that the output it's not too bright.
-            led.write(brightness(gamma(data.iter().cloned()), 10))
+            led.write(brightness(gamma([data].into_iter()), 10))
                 .unwrap();
             delay.delay_millis(20);
         }
